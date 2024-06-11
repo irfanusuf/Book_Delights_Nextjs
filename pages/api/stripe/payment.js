b99e7d6b5401f23f187df86e64c9d10f26280895
unknown <irfanusuf33@gmail.com>
2024-06-11 14:52:00 +0530

    stripe

--- pages/api/stripe/payment.js
+++ pages/api/stripe/payment.js
@@ -1,6 +1,10 @@
 import Stripe from "stripe";
 import { createRouter } from "next-connect";
 
+
+
+
+
 const stripe = new Stripe(
   "sk_test_51POyEoD61yegK70G79EW5QgG3HXdR2qz3s9RiMpUa3d6mebjPqWIS0QG4kW8a3fyRRnpdcx3PUn3YaR5f9GOwX2e00AZRdQDIk"
 );
@@ -14,6 +18,9 @@ apiRouter.use(async (req, res, next) => {
     res.status(405).end(); // Method Not Allowed
   }
 });
+
+
+
 apiRouter.post(async (req, res) => {
   const { items } = req.body;
 
